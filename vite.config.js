bd7ce25ad782d6fb53774d0e6507b5b37cea41fc
Dan E <dan_enright07@hotmail.co.uk>
2022-02-06 21:58:21 +0000

    master push

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,11 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+
   define: {
     'process.env': process.env
   },
+  base: '/indeams/',
   server: {
     port: "3001"
   },
